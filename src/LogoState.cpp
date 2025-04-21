865b637c6c73fb8280a8bc0a88dad757fc3f806e
roninxv <simsjohn15@gmail.com>
2025-04-21 15:25:13 -0700

    Simplify font loading in LogoState render method

--- src/LogoState.cpp
+++ src/LogoState.cpp
@@ -36,62 +36,35 @@ void LogoState::render() {
   SDL_Color titleColor = {255, 255, 255, 255}; // White
   SDL_Color subtitleColor = {200, 200, 200, 255}; // Light gray
   
-  // Check if font was loaded successfully
-  // This is a fallback to ensure we don't crash if font loading failed
-  static bool fontLoaded = false;
-  static bool fontWarningShown = false;
+  // Use existing fonts from the FontManager instead of loading new ones
+  // The GameEngine already loads fonts with ID "fonts_Arial" (from "res/fonts/Arial.ttf")
   
-  if (!fontLoaded) {
-    // Try to load a font if it wasn't loaded in GameEngine initialization
-    const char* fontPaths[] = {
-      "res/fonts/Arial.ttf",
-      "../res/fonts/Arial.ttf",
-      "../../res/fonts/Arial.ttf"
-    };
-    
-    for (const char* path : fontPaths) {
-      if (FontManager::Instance()->loadFont(path, "logo_large", 36)) {
-        fontLoaded = true;
-        // Once we have one font loaded, load a smaller size too
-        FontManager::Instance()->loadFont(path, "logo_small", 24);
-        break;
-      }
-    }
-    
-    if (!fontLoaded && !fontWarningShown) {
-      std::cerr << "Warning: Could not load any fonts for text rendering.\n";
-      fontWarningShown = true;
-    }
-  }
+  // Draw title text
+  FontManager::Instance()->drawText(
+      "FORGE GAME ENGINE", 
+      "fonts_Arial", 
+      (GameEngine::Instance()->getWindowWidth() / 2) - 150,
+      (GameEngine::Instance()->getWindowHeight() / 2) + 200, 
+      titleColor, 
+      GameEngine::Instance()->getRenderer());
   
-  if (fontLoaded) {
-    // Draw title text
-    FontManager::Instance()->drawText(
-        "FORGE GAME ENGINE", 
-        "logo_large", 
-        (GameEngine::Instance()->getWindowWidth() / 2) - 150,
-        (GameEngine::Instance()->getWindowHeight() / 2) + 200, 
-        titleColor, 
-        GameEngine::Instance()->getRenderer());
-    
-    // Draw subtitle text
-    FontManager::Instance()->drawText(
-        "Powered by SDL3", 
-        "logo_small", 
-        (GameEngine::Instance()->getWindowWidth() / 2) - 80,
-        (GameEngine::Instance()->getWindowHeight() / 2) + 240, 
-        subtitleColor, 
-        GameEngine::Instance()->getRenderer());
-    
-    // Draw version text
-    FontManager::Instance()->drawText(
-        "v1.0.0", 
-        "logo_small", 
-        (GameEngine::Instance()->getWindowWidth() / 2) - 30,
-        (GameEngine::Instance()->getWindowHeight() / 2) + 280, 
-        subtitleColor, 
-        GameEngine::Instance()->getRenderer());
-  }
+  // Draw subtitle text
+  FontManager::Instance()->drawText(
+      "Powered by SDL3", 
+      "fonts_Arial", 
+      (GameEngine::Instance()->getWindowWidth() / 2) - 80,
+      (GameEngine::Instance()->getWindowHeight() / 2) + 240, 
+      subtitleColor, 
+      GameEngine::Instance()->getRenderer());
+  
+  // Draw version text
+  FontManager::Instance()->drawText(
+      "v1.0.0", 
+      "fonts_Arial", 
+      (GameEngine::Instance()->getWindowWidth() / 2) - 30,
+      (GameEngine::Instance()->getWindowHeight() / 2) + 280, 
+      subtitleColor, 
+      GameEngine::Instance()->getRenderer());
 }
 
 bool LogoState::exit() {
