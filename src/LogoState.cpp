f0da2f2d5d03878ba1334fffd51dbf1f8df08595
roninxv <simsjohn15@gmail.com>
2025-04-21 10:51:45 -0700

    Fix gamepad initialization and adjust input handling
    Move gamepad subsystem init to InputHandler, reduce logo sound
    volume, and adjust player gamepad sensitivity calculations

--- src/LogoState.cpp
+++ src/LogoState.cpp
@@ -7,23 +7,29 @@ Uint64 stateTimer{0};
 
 bool LogoState::enter() {
   std::cout << "Forge Game Engine - Entering LOGO State" << std::endl;
-  SoundManager::Instance()->playSFX("sfx_logo", 0, 100);
+  SoundManager::Instance()->playSFX("sfx_logo", 0, 5);
   return true;
 }
 
 void LogoState::update() {
-  //std::cout << "Forge Game Engine - Updating LOGO State" << std::endl;
+  // std::cout << "Forge Game Engine - Updating LOGO State" << std::endl;
 
-      stateTimer = SDL_GetTicks();
-      if (stateTimer > 8000) {
-          GameEngine::Instance()->getGameStateManager()->setState("MainMenuState");
-      }
+  stateTimer = SDL_GetTicks();
+  if (stateTimer > 8000) {
+    GameEngine::Instance()->getGameStateManager()->setState("MainMenuState");
   }
+}
 
 void LogoState::render() {
-  //std::cout << "Rendering Main Menu State" << std::endl;
-  TextureManager::Instance()->draw("HammerForgeBanner", (GameEngine::Instance()->getWindowWidth() / 2) - 400, (GameEngine::Instance()->getWindowHeight() / 2) - 352, 727, 352, GameEngine::Instance()->getRenderer());
-  TextureManager::Instance()->draw("ForgeEngine", (GameEngine::Instance()->getWindowWidth() / 2) - 95, (GameEngine::Instance()->getWindowHeight() / 2) + 50, 128, 128, GameEngine::Instance()->getRenderer());
+  // std::cout << "Rendering Main Menu State" << std::endl;
+  TextureManager::Instance()->draw(
+      "HammerForgeBanner", (GameEngine::Instance()->getWindowWidth() / 2) - 400,
+      (GameEngine::Instance()->getWindowHeight() / 2) - 352, 727, 352,
+      GameEngine::Instance()->getRenderer());
+  TextureManager::Instance()->draw(
+      "ForgeEngine", (GameEngine::Instance()->getWindowWidth() / 2) - 95,
+      (GameEngine::Instance()->getWindowHeight() / 2) + 50, 128, 128,
+      GameEngine::Instance()->getRenderer());
 }
 
 bool LogoState::exit() {
