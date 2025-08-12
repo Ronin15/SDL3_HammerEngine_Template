ad9c6528b35bb4cda7c9baf07ef62b2bbe05a616
RoninXV <simsjohn15@gmail.com>
2025-08-12 09:46:26 -0700

    fixe serialization examples and implementation

--- tests/mocks/MockPlayer.cpp
+++ tests/mocks/MockPlayer.cpp
@@ -1,8 +1,61 @@
 /* Copyright (c) 2025 Hammer Forged Games
  * All rights reserved.
  * Licensed under the MIT License - see LICENSE file for details
-*/
+ */
 
 #include "MockPlayer.hpp"
+#include <sstream>
 
-// The implementation is empty because all member functions are defined inline in the header
+// Implementation using BinarySerializer macros for clean, maintainable code
+BEGIN_SERIALIZE(MockPlayer)
+    // Create a temporary stream for using BinarySerial::Writer
+    std::ostringstream tempStream(std::ios::binary);
+    auto streamPtr = std::make_shared<std::ostringstream>(std::move(tempStream));
+    BinarySerial::Writer writer(std::static_pointer_cast<std::ostream>(streamPtr));
+    
+    // Serialize position using Vector2D specialization
+    Vector2D pos = getPosition();
+    SERIALIZE_SERIALIZABLE(writer, pos);
+    
+    // Serialize velocity using Vector2D specialization
+    Vector2D vel = getVelocity();
+    SERIALIZE_SERIALIZABLE(writer, vel);
+    
+    // Serialize textureID string
+    const std::string& textureID = getTextureID();
+    SERIALIZE_STRING(writer, textureID);
+    
+    // Serialize currentStateName string
+    SERIALIZE_STRING(writer, m_currentStateName);
+    
+    // Write the binary data to the original stream
+    std::string binaryData = streamPtr->str();
+    stream.write(binaryData.data(), binaryData.size());
+END_SERIALIZE()
+
+BEGIN_DESERIALIZE(MockPlayer)
+    // Read all data from stream into a string
+    std::string binaryData((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    
+    // Create a stringstream from the binary data
+    auto streamPtr = std::make_shared<std::istringstream>(binaryData, std::ios::binary);
+    BinarySerial::Reader reader(std::static_pointer_cast<std::istream>(streamPtr));
+    
+    // Deserialize position using Vector2D specialization
+    Vector2D pos;
+    DESERIALIZE_SERIALIZABLE(reader, pos);
+    setPosition(pos);
+    
+    // Deserialize velocity using Vector2D specialization
+    Vector2D vel;
+    DESERIALIZE_SERIALIZABLE(reader, vel);
+    setVelocity(vel);
+    
+    // Deserialize textureID string
+    std::string textureID;
+    DESERIALIZE_STRING(reader, textureID);
+    setTextureID(textureID);
+    
+    // Deserialize currentStateName string
+    DESERIALIZE_STRING(reader, m_currentStateName);
+END_DESERIALIZE()
