3244e270642e81011ad4fbb839513568d8c9e646
John Sims <simsjohn15@gmail.com>
2025-11-21 14:02:22 -0800

    test tweak to map accurate decimation timings

--- tests/integration/AICollisionIntegrationTests.cpp
+++ tests/integration/AICollisionIntegrationTests.cpp
@@ -462,9 +462,11 @@ BOOST_AUTO_TEST_CASE(TestAISeparationForces) {
     // Record initial collision query count
     size_t initialQueries = CollisionManager::Instance().getPerfStats().lastPairs;
 
-    // Run simulation for 150 frames (2.5 seconds)
-    std::cout << "Running simulation for 150 frames..." << std::endl;
-    updateSimulation(150, 0.016f);
+    // Run simulation for 250 frames (4.0 seconds)
+    // Extended duration ensures all entities trigger separation 2+ times
+    // given the 2-4 second staggered decimation interval
+    std::cout << "Running simulation for 250 frames..." << std::endl;
+    updateSimulation(250, 0.016f);
 
     // Get final collision query count
     size_t finalQueries = CollisionManager::Instance().getPerfStats().lastPairs;
